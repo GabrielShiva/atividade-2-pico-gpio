0c4f31efa5ca06c48d4fb423a95b628bd54e39fb
Gabriel Shiva Gomes Oliveira <gabrielshivagomes@gmail.com>
2025-01-19 10:49:48 -0300

    Configuração inicial dos pinos para leds e botão

--- tarefa_2_pico_gpio_putty.c
+++ tarefa_2_pico_gpio_putty.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#define LED_RED_PIN 13
+#define LED_GREEN_PIN 11
+#define LED_BLUE_PIN 12
+#define BTN_A_PIN 5
 
+void set_gpio(uint gpio, bool direction, bool is_pull_up_active) {
+    gpio_init(gpio);
+    gpio_set_dir(gpio, direction);
 
-int main()
-{
+    if (is_pull_up_active) {
+        gpio_pull_up(gpio);
+    }
+}
+
+int main() {
     stdio_init_all();
 
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
-    }
+    set_gpio(LED_RED_PIN, GPIO_OUT, false);    
+    set_gpio(LED_GREEN_PIN, GPIO_OUT, false);    
+    set_gpio(LED_BLUE_PIN, GPIO_OUT, false);    
+    set_gpio(BTN_A_PIN, GPIO_IN, true);    
 }
